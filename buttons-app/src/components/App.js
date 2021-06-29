a5a4bf9fb71f20e1e160c74673d9141c74beecfe
julian garcia <juliancamilogarciaescobar7@gmail.com>
2021-06-29 11:47:54 -0500

    estructura de archivos

---
+++ buttons-app/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hola</p>
+        <button>uno</button>
+        <button>dos tres</button>
+        <button>dos tres</button>
+        <button>dos tres</button>
+        <button>dos tres</button>
+        <button>dos tres</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ buttons-app/src/components/First.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+
+Class Buttons = React.createClass({})
