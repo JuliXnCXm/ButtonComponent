ad405537e891e7d85404b3c4ba3a23da11b2c110
JulianDev <juliancamilogarciaescobar7@gmail.com>
2023-01-06 23:13:08 -0500

    fixing margin and repo name

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -13,7 +13,7 @@ class Buttons extends React.Component {
   render() {
     return (
       <div className="buttons">
-          <h1 type="text">Buttons</h1>
+          <h1>Buttons</h1>
           <DefaultButton/>
           <OutlineButton/>
           <TextButton/>
